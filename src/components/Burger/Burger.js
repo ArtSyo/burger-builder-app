c6afe4e9d867645172cf2c50636c64475b056825
Artyom <seminartem98@mail.ru>
2020-11-18 13:42:28 +0700

    Simple dynamic in burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import "./Burger.css";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingKey) => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredients key={ingKey + i} type={ingKey} />;
+      });
+    }
+  );
+  console.log(transformedIngredients);
+
   return (
     <div className="Burger">
       <BurgerIngredients type="bread-top" />
-      <BurgerIngredients type="cheese" />
-      <BurgerIngredients type="meat" />
+      {transformedIngredients}
       <BurgerIngredients type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Burger from '../../components/Burger/Burger';
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
-    render() {
-        return (
-            <>
-            <div>
-                <Burger />
-            </div>
-            <div>Build Components</div>
-            </>
-        )
-    }
+  state = {
+    ingredients: {
+      bacon: 1,
+      salad: 1,
+      cheese: 2,
+      meat: 1,
+    },
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <Burger ingredients={this.state.ingredients}/>
+        </div>
+        <div>Build Components</div>
+      </>
+    );
+  }
 }
 
 export default BurgerBuilder;
