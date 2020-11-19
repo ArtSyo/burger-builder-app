4e884dcfb83011c52f3e3795beadf1c5dae558b9
Artyom <seminartem98@mail.ru>
2020-11-19 14:58:18 +0700

    Some improvments in withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,13 +9,13 @@ const withErrorHandler = (WrapperComponent, axios) => {
     };
 
     componentDidMount() {
-      axios.interceptors.response.use((req) => {
+      this.requestInterceptor = axios.interceptors.response.use((req) => {
         this.setState({
           error: null,
         });
         return req;
       });
-      axios.interceptors.response.use(
+      this.responsetInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({
@@ -23,6 +23,11 @@ const withErrorHandler = (WrapperComponent, axios) => {
           });
         }
       );
+    }
+
+    componentWillUnmount() {
+        axios.interceptors.request.eject(this.requestInterceptor);
+        axios.interceptors.response.eject(this.responsetInterceptor);
     }
 
     errorConfirmedHandler = () => {
