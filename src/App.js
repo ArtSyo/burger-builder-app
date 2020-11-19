5d2b95d5290beac363c082977fbbc3d7479a707f
Artyom <seminartem98@mail.ru>
2020-11-19 16:40:03 +0700

    Added checkout components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
+import Checkout from "./containers/Checkout/Checkout";
+
 function App() {
   return (
     <>
       <div className="App">
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     </>
