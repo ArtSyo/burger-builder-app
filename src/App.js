1c91a1b6bb470ec3543fdc3fac89b631ad3e8308
Artyom <seminartem98@mail.ru>
2020-11-19 19:54:30 +0700

    Created orders container and order component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Orders from './containers/Checkout/Orders/Orders';
 
 import Checkout from "./containers/Checkout/Checkout";
 
@@ -13,6 +14,7 @@ function App() {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route exact path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
---
+++ src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Order.css';
+
+const Order = (props) => {
+    return(
+        <div className='Order'>
+            <p>Ingredients ...</p>
+            <p>Price: <strong>$ 0000</strong></p>
+        </div>
+    )
+}
+
+export default Order;
